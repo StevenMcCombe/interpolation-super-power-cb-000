d0a8c41ec6f0bbafaea5defa769207ec9b53dc35
Steven McCombe <stevenmccombe@hotmail.co.uk>
2017-08-15 23:12:32 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
-# Write your #display_rainbow method here
+# Wricte your #display_rainbow method here
+#colors = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
+#def display_rainbow(colors)
+#
+#puts colors
+#
+#end
+
+#display_rainbow(colors)
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+ def display_rainbow(aura)
+   puts "R: #{aura[0]}, O: #{aura[1]}, Y: #{aura[2]}, G: #{aura[3]}, B: #{aura[4]}, I: #{aura[5]}, V: #{aura[6]}"
+ end
